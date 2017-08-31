1fdc533f6b26d1fb436ce42c95e629a70d1f83c5
Sarmacid <steamroller@wedermark.com>
2017-08-31 22:17:18 +1000

    Cleaned up a bit.

--- evaporate/config.py
+++ evaporate/config.py
@@ -1,10 +1,6 @@
 import ConfigParser
 import os
-
-CONFIG_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                           '../config/config.cfg'))
-config = ConfigParser.ConfigParser()
-config.read(CONFIG_FILE)
+from sys import exit
 
 
 def get_option(option, option_type='str'):
@@ -23,11 +19,24 @@ def get_option(option, option_type='str'):
 
 
 def get_section_name():
+    """
+    Returns name of the section in the config file.
+    """
     return 'Evaporate'
 
 
 def get_path(path_type=None, add=None):
-    path = os.path.join(os.getenv("HOME"), 'evaporate')
+    """
+    Can return the base path on its own, or can add either of the following to
+    it:
+        * "mp3"
+        * Second argument
+        * "mp3" + second argument
+    """
+    path = get_option('PATH')
+    if not os.path.isdir(path):
+        print 'Path "' + path + '" does not exist.'
+        exit()
     if path_type == 'mp3':
         path = os.path.join(path, 'mp3')
     if add:
@@ -38,3 +47,19 @@ def get_path(path_type=None, add=None):
 def get_db_path():
     path = get_path(None, get_option('DB_FILENAME'))
     return path
+
+
+def check_config_file():
+    """
+    Check if the config file exists, exits otherwise.
+    """
+    if not os.path.isfile(CONFIG_FILE):
+        print 'Config file not found. Exiting...'
+        exit()
+
+
+CONFIG_FILE = os.path.abspath(os.path.join(os.getenv("HOME"),
+                                           '.evaporate/config.cfg'))
+check_config_file()
+config = ConfigParser.ConfigParser()
+config.read(CONFIG_FILE)
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='evaporate',
+      version='0.1',
+      description='Turn a youtube playlist into an audio podcast.',
+      long_description='Evaporate takes a youtube playlist as an argument, '
+      'downloads the videos in it as mp3s and creates an XML file that would '
+      'act as an RSS feed.',
+      url='https://github.com/Sarmacid/evaporate',
+      author='Sarmacid',
+      license='GPLv3',
+      packages=['evaporate'],
+      install_requires=[
+          'PyRSS2Gen',
+          'youtube_dl',
+      ],
+      scripts=['bin/evaporate'],
+      zip_safe=False)
